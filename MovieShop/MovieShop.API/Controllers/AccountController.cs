efe9c16e947d333f441b5306c3bf176b1f27b142
Sherry <asdfg5665@gmail.com>
2021-05-14 08:56:32 -0700

    [0514] class materials - JWT

--- MovieShop/MovieShop.API/Controllers/AccountController.cs
+++ MovieShop/MovieShop.API/Controllers/AccountController.cs
@@ -6,6 +6,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using ApplicationCore.Models.Response;
+using System.Security.Claims;
+using Microsoft.Extensions.Configuration;
+using System.Text;
 
 namespace MovieShop.API.Controllers
 {
@@ -14,10 +20,12 @@ namespace MovieShop.API.Controllers
     public class AccountController : ControllerBase
     {
         private readonly IUserService _userService;
+        private readonly IConfiguration _configuration;
 
-        public AccountController(IUserService userService)
+        public AccountController(IUserService userService, IConfiguration configuration)
         {
             _userService = userService;
+            _configuration = configuration;
         }
 
         [HttpPost]
@@ -44,10 +52,52 @@ namespace MovieShop.API.Controllers
             var user = await _userService.ValidateUser(requestModel.Email, requestModel.Password);
             if(user != null)
             {
-                return Ok(user);
+                //if user enter valid un/pw
+                //create JWT Token
+                var token = GenerateJWT(user);
+                return Ok(new { token = token });
             }
 
-            return ValidationProblem("Email or Password is incorrect!");
+            //return ValidationProblem("Email or Password is incorrect!");
+            return Unauthorized();
+        }
+
+        private string GenerateJWT(UserLoginResponseModel model)
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.NameIdentifier, model.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, model.Email),
+                new Claim(JwtRegisteredClaimNames.GivenName, model.FirstName),
+                new Claim(JwtRegisteredClaimNames.FamilyName, model.LastName)
+            };
+            var identityClaims = new ClaimsIdentity();
+            identityClaims.AddClaims(claims);
+
+            //read the secret key from appsettings, make sure secret key is unique and not guessable
+            //In real world we use something like Azure KeyValue to store any secrets of application
+            var secretKey = _configuration["JwtSettings:SecretKey"];
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+
+            //get the expiration time of token
+            var expires = DateTime.UtcNow.AddDays(_configuration.GetValue<int>("JwtSettings:Expiration"));
+
+            //pick an hashing algorithm
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);
+
+            //create the token objects that you will use to store all information
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var tokenDescriptor = new SecurityTokenDescriptor() { 
+                Subject = identityClaims,
+                Expires = expires,
+                SigningCredentials = credentials,
+                Issuer = _configuration["JwtSettings:Issuer"],
+                Audience = _configuration["JwtSettings:Audience"]
+            };
+
+            var encodeJWT = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(encodeJWT);
         }
     }
 }
